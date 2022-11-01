717e63d4f90b877c1993014e9bb7f864fbc73ef6
immortal_sniper1 <38986383+immortal-sniper1@users.noreply.github.com>
2022-11-01 11:30:52 +0200

    Update CardInfo_alt_lib.ino

--- SW/CardInfo_alt_lib/CardInfo_alt_lib.ino
+++ SW/CardInfo_alt_lib/CardInfo_alt_lib.ino
@@ -1,25 +1,25 @@
 /****************************************************************************************************************************
   CardInfo.ino
-  
+
   For all RP2040 boads using Arduimo-mbed or arduino-pico core
-  
+
   RP2040_SD is a library enable the usage of SD on RP2040-based boards
-  
+
   This Library is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
   as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
   This Library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License along with the Arduino SdFat Library.  
+  You should have received a copy of the GNU General Public License along with the Arduino SdFat Library.
   If not, see <http://www.gnu.org/licenses/>.
-  
+
   Based on and modified from  Arduino SdFat Library (https://github.com/arduino/Arduino)
-  
+
   (C) Copyright 2009 by William Greiman
   (C) Copyright 2010 SparkFun Electronics
   (C) Copyright 2021 by Khoi Hoang
-  
+
   Built by Khoi Hoang https://github.com/khoih-prog/RP2040_SD
   Licensed under GPL-3.0 license
  *****************************************************************************************************************************/
@@ -42,41 +42,49 @@
    ** SCK  - pin 2
 */
 
+
+
 /*
-#define PIN_SPI_SS 9
-#define PIN_SPI_MISO 12
-#define PIN_SPI_MOSI 11
-#define PIN_SPI_SCK 10
-*/
 #define PIN_SPI_SC 21
 #define PIN_SPI_MISO 20
 #define PIN_SPI_MOSI 19
 #define PIN_SPI_SCK 18
-
+*/
 
 
 
 #if !defined(ARDUINO_ARCH_RP2040)
-  #error For RP2040 only
+#error For RP2040 only
 #endif
 
 #if defined(ARDUINO_ARCH_MBED)
-  
-  #define PIN_SD_MOSI       PIN_SPI_MOSI
-  #define PIN_SD_MISO       PIN_SPI_MISO
-  #define PIN_SD_SCK        PIN_SPI_SCK
-  #define PIN_SD_SS         PIN_SPI_SS
+
+#define PIN_SD_MOSI       PIN_SPI_MOSI
+#define PIN_SD_MISO       PIN_SPI_MISO
+#define PIN_SD_SCK        PIN_SPI_SCK
+#define PIN_SD_SS         PIN_SPI_SS
 
 #else
 
-  #define PIN_SD_MOSI       PIN_SPI0_MOSI
-  #define PIN_SD_MISO       PIN_SPI0_MISO
-  #define PIN_SD_SCK        PIN_SPI0_SCK
-  #define PIN_SD_SS         PIN_SPI0_SS
-  
+#define PIN_SD_MOSI       PIN_SPI0_MOSI
+#define PIN_SD_MISO       PIN_SPI0_MISO
+#define PIN_SD_SCK        PIN_SPI0_SCK
+#define PIN_SD_SS         PIN_SPI0_SS
+
 #endif
 
-#define _RP2040_SD_LOGLEVEL_       1
+#define PIN_SPI_SS 5
+#define PIN_SPI_MISO 4
+#define PIN_SPI_MOSI 3
+#define PIN_SPI_SCK 2
+
+#define PIN_SD_MOSI       3
+#define PIN_SD_MISO       4
+#define PIN_SD_SCK        2
+#define PIN_SD_SS         5
+
+
+#define _RP2040_SD_LOGLEVEL_       0
 
 #include <SPI.h>
 #include <RP2040_SD.h>
@@ -88,42 +96,42 @@ File root;
 
 const int chipSelect = PIN_SD_SS;
 
-void printDirectory(File dir, int numTabs) 
+void printDirectory(File dir, int numTabs)
 {
-  while (true) 
+  while (true)
   {
     File entry =  dir.openNextFile();
 
-    if (! entry) 
+    if (! entry)
     {
       // no more files
       break;
     }
-    
-    for (uint8_t i = 0; i < numTabs; i++) 
+
+    for (uint8_t i = 0; i < numTabs; i++)
     {
       Serial.print('\t');
     }
-    
+
     Serial.print(entry.name());
-    
-    if (entry.isDirectory()) 
+
+    if (entry.isDirectory())
     {
       Serial.println("/");
       printDirectory(entry, numTabs + 1);
-    } 
-    else 
+    }
+    else
     {
       // files have sizes, directories do not
       Serial.print("\t\t");
       Serial.println(entry.size(), DEC);
     }
-    
+
     entry.close();
   }
 }
 
-void setup() 
+void setup()
 {
   // Open serial communications and wait for port to open:
   Serial.begin(115200);
@@ -134,12 +142,13 @@ void setup()
 #if defined(ARDUINO_ARCH_MBED)
   Serial.print("Starting SD Card CardInfo on MBED ");
 #else
-  Serial.print("Starting SD Card CardInfo on ");
+  Serial.print("Starting SD Card CardInfo on ");  
+  Serial.print("test does it even upload well?");
 #endif
-  
+
   Serial.println(BOARD_NAME);
   Serial.println(RP2040_SD_VERSION);
-  
+
   Serial.print("Initializing SD card with SS = ");  Serial.println(PIN_SD_SS);
   Serial.print("SCK = ");   Serial.println(PIN_SD_SCK);
   Serial.print("MOSI = ");  Serial.println(PIN_SD_MOSI);
@@ -147,15 +156,15 @@ void setup()
 
   // we'll use the initialization code from the utility libraries
   // since we're just testing if the card is working!
-  if (!card.init(SPI_HALF_SPEED, chipSelect)) 
+  if (!card.init(SPI_HALF_SPEED, chipSelect))
   {
     Serial.println("initialization failed. Things to check:");
     Serial.println("* is a card inserted?");
     Serial.println("* is your wiring correct?");
     Serial.println("* did you change the chipSelect pin to match your shield or module?");
     while (1);
-  } 
-  else 
+  }
+  else
   {
     Serial.println("Wiring is correct and a card is present.");
   }
@@ -163,24 +172,24 @@ void setup()
   // print the type of card
   Serial.println();
   Serial.print("Card type:         ");
-  
-  switch (card.type()) 
+
+  switch (card.type())
   {
-    case SD_CARD_TYPE_SD1:
-      Serial.println("SD1");
-      break;
-    case SD_CARD_TYPE_SD2:
-      Serial.println("SD2");
-      break;
-    case SD_CARD_TYPE_SDHC:
-      Serial.println("SDHC");
-      break;
-    default:
-      Serial.println("Unknown");
+  case SD_CARD_TYPE_SD1:
+    Serial.println("SD1");
+    break;
+  case SD_CARD_TYPE_SD2:
+    Serial.println("SD2");
+    break;
+  case SD_CARD_TYPE_SDHC:
+    Serial.println("SDHC");
+    break;
+  default:
+    Serial.println("Unknown");
   }
 
   // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
-  if (!volume.init(card)) 
+  if (!volume.init(card))
   {
     Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
     while (1);
@@ -211,12 +220,12 @@ void setup()
   Serial.print("Volume size (Gb):  ");
   Serial.println((float)volumesize / 1024.0);
 
-  if (!SD.begin(PIN_SD_SS)) 
+  if (!SD.begin(PIN_SD_SS))
   {
     Serial.println("Initialization failed!");
     return;
   }
-  
+
   Serial.println("Initialization done.");
 
   Serial.println("\nFiles found on the card (name, date and size in bytes): ");
@@ -226,6 +235,6 @@ void setup()
   printDirectory(root, 0);
 }
 
-void loop() 
+void loop()
 {
 }
